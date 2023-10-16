9d12cd99cc38f351cbbcb7a6e2455c8bb062a8cd
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-16 19:24:01 +0300

    FEAT:  HOW TO ADD REACT-QUERY

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		//* FOR HTTP GET REQUESTS
+
+		queries: {
+			// don't refetch when i change tab and come again
+			refetchOnWindowFocus: false,
+		},
+
+		//* FOR HTTP POST PUT PATCH DELETE REQUESTS
+
+		mutations: {},
+	},
+})
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</React.StrictMode>
 )
