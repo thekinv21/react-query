c919f0f56d3d3f2fa89c45332fa912bf05fd168c
Vadim Kiniabaev <thekinv21@gmail.com>
2023-10-16 21:48:18 +0300

    FIX: dirname bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { fileURLToPath } from 'url'
+// Eklendi
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(import.meta.url) // __filename tanımlandı
+const __dirname = path.dirname(__filename) // __dirname hesaplandı
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': path.resolve(path.__dirname, './src')
+			'@': path.resolve(__dirname, './src') // __dirname kullanıldı
 		}
 	}
 })
